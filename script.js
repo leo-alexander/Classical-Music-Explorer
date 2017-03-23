aec979132ec485d65fd8522b7272bab6afa0b017
EDA <eda@mac-mini-4.local>
2017-03-23 17:44:12 +1300

    CSS functionality

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-// d3.select("body").transition()
-//     .style("background-color", "black");
-//
-//
-// d3.partition()
+const $ = window.$
+
+const periods = [{
+  id: 'show-renaissance',
+  bar: 'renaissance-bar',
+  box: 'renaissance-box'
+}, {
+  id: 'show-baroque',
+  bar: 'baroque-bar',
+  box: 'baroque-box'
+}, {
+  id: 'show-classical',
+  bar: 'classical-bar',
+  box: 'classical-box'
+}, {
+  id: 'show-romantic',
+  bar: 'romantic-bar',
+  box: 'romantic-box'
+}, {
+  id: 'show-modern',
+  bar: 'modern-bar',
+  box: 'modern-box'
+}]
+
+function addListeners () {
+  periods.map(period => {
+    $('#' + period.id).click(function () {
+      showPeriod(period)
+      scrollToPeriod(period)
+    })
+  })
+}
+addListeners()
+
+function showPeriod (selectedPeriod) {
+  const hidePeriods = periods.filter(period => period.id !== selectedPeriod.id)
+  hidePeriods.map(period => {
+    $('.' + period.bar).removeClass('active-bar')
+    $('.' + period.box).removeClass('active-info')
+  })
+  $('.' + selectedPeriod.bar).toggleClass('active-bar')
+  $('.' + selectedPeriod.box).toggleClass('active-info')
+}
+
+function scrollToPeriod (selectedPeriod) {
+  console.log(selectedPeriod)
+  $('html, body').animate({ scrollTop: $('#' + selectedPeriod.id).offset().top - 50 }, 400)
+}
+
+
+
+window.setTimeout(function () {
+  var elements = document.getElementsByClassName('infobox')
+  Array.prototype.map.call(elements, function (element) {
+    element.style.visibility = 'visible'
+  })
+}, 1000)
